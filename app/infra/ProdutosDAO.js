e01c83d1363933a2503d4485b9a40875c7b100e0
Isabela Neri dos Santos <isabelandss@gmail.com>
2017-09-08 14:42:42 -0300

    Aula 4

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -6,6 +6,10 @@ ProdutosDAO.prototype.lista = function(callback) {
     this._connection.query("SELECT * FROM LIVROS", callback);
 }
 
+ProdutosDAO.prototype.salva = function(produto, callback) {
+    this._connection.query("INSERT INTO LIVROS SET ?", produto, callback);
+}
+
 module.exports = function() {
     return ProdutosDAO;
 }
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,11 +1,44 @@
 module.exports = function(app) {
-    app.get("/produtos", function(req, response) {
-        var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function(err, result){
+    var listaProdutos = function(req, response) {
+        var connection = app.infra.connectionFactory();
+        var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+
+        ProdutosDAO.lista(function(err, result){
             response.render("produtos/lista", {lista: result});
         });
         connection.end();
+    }
+
+    app.get("/produtos", function(req, response) {
+        var connection = app.infra.connectionFactory();
+        var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+
+        ProdutosDAO.lista(function(err, result){
+            response.format({
+                html: function() {
+                    response.render("produtos/lista", {lista: result});
+                },
+                json: function() {
+                    response.render(result);
+                }
+            });
+        });
+        connection.end();
+    });
+
+    app.get("/produtos/form", function(req, response) {
+        response.render("produtos/form");
+    });
+    
+    app.post("/produtos", function(req, response) {
+        var connection = app.infra.connectionFactory();
+        var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+
+        var produto = req.body;
+
+        ProdutosDAO.salva(produto, function(erros, resultados) {
+            response.redirect("/produtos");
+        });
     });
 }
